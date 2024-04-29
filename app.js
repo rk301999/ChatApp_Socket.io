da73d484e1e809703e3059dd263deed1e64d3a62
Ritesh <rk301999@gmail.com>
2024-04-29 13:05:10 +0530

    UI added , all functionality working

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
-const express = require("express") //express is a module
-const app = express() //express is a function 
-const path = require("path")
-require("dotenv/config")
-
-console.log(process.env);
+const express = require('express')
+const path = require('path')
+const app = express()
 const PORT = process.env.PORT || 4000
+const server = app.listen(PORT, () => console.log(`💬 server on port ${PORT}`))
 
-app.use(express.static(path.join(__dirname,'public')))
+const io = require('socket.io')(server)
 
-const server = app.listen(PORT , ()=>{
-    console.log(`💬 server is on port ${PORT}`);
-})
+app.use(express.static(path.join(__dirname, 'public')))
 
+let socketsConected = new Set()
+
+io.on('connection', onConnected)
+
+function onConnected(socket) {
+  console.log('Socket connected', socket.id)
+  socketsConected.add(socket.id)
+  io.emit('clients-total', socketsConected.size)
+
+  socket.on('disconnect', () => {
+    console.log('Socket disconnected', socket.id)
+    socketsConected.delete(socket.id)
+    io.emit('clients-total', socketsConected.size)
+  })
+
+  socket.on('message', (data) => {
+    // console.log(data)
+    socket.broadcast.emit('chat-message', data)
+  })
+
+  socket.on('feedback',(data)=>{
+    socket.broadcast.emit('feedback',data);
+  })
+}
